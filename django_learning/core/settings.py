c3c9d28d258efcb2b9fd5f3896105b763055d644
Manu Kumar Rajput <rajputmanu2024@gmail.com>
2026-07-15 00:55:11 +0530

    Remove Windows-only import

--- django_learning/core/settings.py
+++ django_learning/core/settings.py
@@ -30,10 +30,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
 
 # Application definition
 
